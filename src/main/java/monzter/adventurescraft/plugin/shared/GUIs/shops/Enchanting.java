5e363b0c7991f3af98e50e8d47d35b18a5ef1d38
Dancull47 <Dancull47@gmail.com>
2021-05-25 10:21:29 -0400

    - Mostly finished Enchanting

--- src/main/java/monzter/adventurescraft/plugin/shared/GUIs/shops/Enchanting.java
+++ src/main/java/monzter/adventurescraft/plugin/shared/GUIs/shops/Enchanting.java
@@ -32,6 +32,7 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -54,6 +55,7 @@ public class Enchanting extends BaseCommand {
             Material.GOLDEN_AXE, Material.GOLDEN_HOE, Material.GOLDEN_PICKAXE, Material.GOLDEN_SHOVEL,
             Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SHOVEL,
             Material.NETHERITE_AXE, Material.NETHERITE_HOE, Material.NETHERITE_PICKAXE, Material.NETHERITE_SHOVEL);
+    private final DecimalFormat df = new DecimalFormat("###.####");
 
     public Enchanting(AdventuresCraft plugin, SoundManager soundManager, GUIHelper guiHelper, ConsoleCommand consoleCommand, Economy economy, FullInventory fullInventory, MMOItemsGive mmoItemsGive, NumberFormat numberFormat) {
         this.plugin = plugin;
@@ -89,6 +91,13 @@ public class Enchanting extends BaseCommand {
                 display.addItem(new GuiItem(luck(exp, calculateEnchantments(player, "Luck") + 1), e -> player.sendMessage("1")), 3, 0);
                 display.addItem(new GuiItem(explosive(exp, calculateEnchantments(player, "Explosive") + 1), e -> player.sendMessage("1")), 4, 0);
                 display.addItem(new GuiItem(explosiveChance(exp, calculateEnchantments(player, "Explosive Chance") + 1), e -> player.sendMessage("1")), 5, 0);
+
+                display.addItem(new GuiItem(randomizer(exp, calculateEnchantments(player, "Randomizer") + 1), e -> player.sendMessage("1")), 2, 1);
+                display.addItem(new GuiItem(treasurer(exp, calculateEnchantments(player, "Treasurer") + 1), e -> player.sendMessage("1")), 3, 1);
+                display.addItem(new GuiItem(midasTouch(exp, calculateEnchantments(player, "Midas Touch") + 1), e -> player.sendMessage("1")), 4, 1);
+
+                display.addItem(new GuiItem(statTracker(exp, calculateEnchantments(player, "Stat Tracker") + 1), e -> player.sendMessage("1")), 3, 2);
+
 //            display2.addItem(new GuiItem(hatchEgg(Integer.valueOf(exp), enchantment), e -> {
 //                if (e.isLeftClick())
 //                    player.performCommand("hatch " + enchantment.getId());
@@ -119,48 +128,95 @@ public class Enchanting extends BaseCommand {
         TextComponent[] experience = new TextComponent[]{
                 Component.text(ChatColor.GRAY + "Increase your " + StatsDisplay.EXPERIENCE_MULTIPLIER.getName()),
                 Component.text(ChatColor.GRAY + "by " + ChatColor.GREEN + "+" + Double.valueOf(enchantmentLevel * Enchantments.Experience.getIncrease()) + ChatColor.GRAY + " while mining with this tool!")};
-        return enchantment(balance, ChatColor.GREEN + "Experience ", enchantmentLevel, experience, (Integer.valueOf(enchantmentLevel * Enchantments.Experience.getPrice())));
+        if (enchantmentLevel - 1 < Enchantments.Experience.getMaxLevel())
+            return enchantment(balance, ChatColor.GREEN + "Experience ", enchantmentLevel, experience, (Integer.valueOf(enchantmentLevel * Enchantments.Experience.getPrice())), false);
+        return enchantment(balance, ChatColor.GREEN + "Experience ", enchantmentLevel, experience, (Integer.valueOf(enchantmentLevel * Enchantments.Experience.getPrice())), true);
     }
 
     private ItemStack petExperience(int balance, int enchantmentLevel) {
         TextComponent[] petExperience = new TextComponent[]{
                 Component.text(ChatColor.GRAY + "Increase your " + StatsDisplay.PET_EXPERIENCE_MULTIPLIER.getName()),
                 Component.text(ChatColor.GRAY + "by " + ChatColor.GREEN + "+" + Double.valueOf(enchantmentLevel * Enchantments.PetExperience.getIncrease()) + ChatColor.GRAY + " while mining with this tool!")};
-        return enchantment(balance, ChatColor.AQUA + "Pet Experience ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.PetExperience.getPrice())));
+        if (enchantmentLevel - 1 < Enchantments.PetExperience.getMaxLevel())
+            return enchantment(balance, ChatColor.AQUA + "Pet Experience ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.PetExperience.getPrice())), false);
+        return enchantment(balance, ChatColor.AQUA + "Pet Experience ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.PetExperience.getPrice())), true);
     }
 
     private ItemStack luck(int balance, int enchantmentLevel) {
         TextComponent[] petExperience = new TextComponent[]{
                 Component.text(ChatColor.GRAY + "Increase your chances of finding " + ChatColor.GREEN + "Lootboxes "),
                 Component.text(ChatColor.GRAY + "by " + ChatColor.GREEN + Double.valueOf(enchantmentLevel * Enchantments.Luck.getIncrease()) + "x" + ChatColor.GRAY + ", while mining with this tool!")};
-        return enchantment(balance, ChatColor.YELLOW + "Luck ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.Luck.getPrice())));
+        if (enchantmentLevel - 1 < Enchantments.Luck.getMaxLevel())
+            return enchantment(balance, ChatColor.YELLOW + "Luck ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.Luck.getPrice())), false);
+        return enchantment(balance, ChatColor.YELLOW + "Luck ", enchantmentLevel, petExperience, (Integer.valueOf(enchantmentLevel * Enchantments.Luck.getPrice())), true);
     }
 
     private ItemStack explosive(int balance, int enchantmentLevel) {
         TextComponent[] explosive = new TextComponent[]{
                 Component.text(ChatColor.GRAY + "Occasionally break " + ChatColor.GREEN + "+" + enchantmentLevel + ChatColor.GRAY + " nearby"),
                 Component.text(ChatColor.GRAY + "blocks, while mining with this tool!")};
-        return enchantment(balance, ChatColor.DARK_RED + "Explosive ", enchantmentLevel, explosive, (Integer.valueOf(enchantmentLevel * Enchantments.Explosive.getPrice())));
+        if (enchantmentLevel - 1 < Enchantments.Explosive.getMaxLevel())
+            return enchantment(balance, ChatColor.DARK_RED + "Explosive ", enchantmentLevel, explosive, (Integer.valueOf(enchantmentLevel * Enchantments.Explosive.getPrice())), false);
+        return enchantment(balance, ChatColor.DARK_RED + "Explosive ", enchantmentLevel, explosive, (Integer.valueOf(enchantmentLevel * Enchantments.Explosive.getPrice())), true);
     }
 
     private ItemStack explosiveChance(int balance, int enchantmentLevel) {
         TextComponent[] explosiveChance = new TextComponent[]{
                 Component.text(ChatColor.GRAY + "Increase the chance of " + ChatColor.DARK_RED + "Explosive " + ChatColor.GRAY + "occurring"),
-                Component.text(ChatColor.GRAY + "by " + ChatColor.GREEN + Double.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getIncrease() * 10) + "% " + ChatColor.GRAY + "while mining with this tool!")};
-        return enchantment(balance, ChatColor.RED + "Explosive Chance ", enchantmentLevel, explosiveChance, (Integer.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getPrice())));
+                Component.text(ChatColor.GRAY + "by " + ChatColor.GREEN + df.format(Double.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getIncrease() * 10)) + "% " + ChatColor.GRAY + "while mining with this tool!")};
+        if (enchantmentLevel - 1 < Enchantments.ExplosiveChance.getMaxLevel())
+            return enchantment(balance, ChatColor.RED + "Explosive Chance ", enchantmentLevel, explosiveChance, (Integer.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getPrice())), false);
+        return enchantment(balance, ChatColor.RED + "Explosive Chance ", enchantmentLevel, explosiveChance, (Integer.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getPrice())), true);
     }
 
     private ItemStack randomizer(int balance, int enchantmentLevel) {
         TextComponent[] randomizer = new TextComponent[]{
-                Component.text(ChatColor.GRAY + "Increase the chance of spawning a" + ChatColor.GOLD + "valuable block"),
-                Component.text(ChatColor.GRAY + "above you by " + ChatColor.GREEN + Double.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getIncrease() * 10) + "% " + ChatColor.GRAY + "while mining with this tool!")};
-        return enchantment(balance, ChatColor.RED + "Explosive Chance ", enchantmentLevel, randomizer, (Integer.valueOf(enchantmentLevel * Enchantments.ExplosiveChance.getPrice())));
+                Component.text(ChatColor.GRAY + "Increase the chance of spawning a" + ChatColor.DARK_GREEN + " valuable block"),
+                Component.text(ChatColor.GRAY + "above you by " + ChatColor.GREEN + df.format(Double.valueOf(enchantmentLevel * Enchantments.Randomizer.getIncrease() * 10)) + "% " + ChatColor.GRAY + "while mining with this tool!")};
+        if (enchantmentLevel - 1 < Enchantments.Randomizer.getMaxLevel())
+            return enchantment(balance, ChatColor.DARK_GREEN + "Randomizer ", enchantmentLevel, randomizer, (Integer.valueOf(enchantmentLevel * Enchantments.Randomizer.getPrice())), false);
+        return enchantment(balance, ChatColor.DARK_GREEN + "Randomizer ", enchantmentLevel, randomizer, (Integer.valueOf(enchantmentLevel * Enchantments.Randomizer.getPrice())), true);
     }
 
-    private ItemStack enchantment(int balance, String enchantmentName, int enchantmentLevel, TextComponent[] enchantmentLore, int enchantmentPrice) {
-        ItemStack enchantmentItem = new ItemStack(Material.ENCHANTED_BOOK);
+    private ItemStack treasurer(int balance, int enchantmentLevel) {
+        TextComponent[] treasurer = new TextComponent[]{
+                Component.text(ChatColor.GRAY + "Increase the chance of spawning a" + ChatColor.GOLD + " special treasure"),
+                Component.text(ChatColor.GRAY + "below you by " + ChatColor.GREEN + df.format(Double.valueOf(enchantmentLevel * Enchantments.Treasurer.getIncrease() * 10)) + "% " + ChatColor.GRAY + "while mining with this tool!")};
+        if (enchantmentLevel - 1 < Enchantments.Treasurer.getMaxLevel())
+            return enchantment(balance, ChatColor.GOLD + "Treasurer ", enchantmentLevel, treasurer, (Integer.valueOf(enchantmentLevel * Enchantments.Treasurer.getPrice())), false);
+        return enchantment(balance, ChatColor.GOLD + "Treasurer ", enchantmentLevel, treasurer, (Integer.valueOf(enchantmentLevel * Enchantments.Treasurer.getPrice())), true);
+    }
+
+    private ItemStack midasTouch(int balance, int enchantmentLevel) {
+        TextComponent[] midasTouch = new TextComponent[]{
+                Component.text(ChatColor.GRAY + "Increase the chance of gaining a random amount "),
+                Component.text(ChatColor.GRAY + "of " + StatsDisplay.MONEY_AMOUNT.getName() + ChatColor.GRAY + " by " + ChatColor.GREEN + df.format(Double.valueOf(enchantmentLevel * Enchantments.MidasTouch.getIncrease() * 10)) + "% " + ChatColor.GRAY + "while mining with this tool!")};
+        if (enchantmentLevel - 1 < Enchantments.MidasTouch.getMaxLevel())
+            return enchantment(balance, ChatColor.YELLOW + "Midas Touch ", enchantmentLevel, midasTouch, (Integer.valueOf(enchantmentLevel * Enchantments.MidasTouch.getPrice())), false);
+        return enchantment(balance, ChatColor.YELLOW + "Midas Touch ", enchantmentLevel, midasTouch, (Integer.valueOf(enchantmentLevel * Enchantments.MidasTouch.getPrice())), true);
+    }
+
+    private final TextComponent[] statTracker = new TextComponent[]{
+            Component.text(ChatColor.GRAY + "Track the amount of blocks broken with your"),
+            Component.text(ChatColor.GRAY + "tool, earning " + ChatColor.GREEN + "rewards" + ChatColor.GRAY + " while mining!")};
+
+    private ItemStack statTracker(int balance, int enchantmentLevel) {
+        if (enchantmentLevel - 1 < Enchantments.StatTracker.getMaxLevel())
+            return enchantment(balance, ChatColor.GREEN.toString() + ChatColor.BOLD + "Stat Tracker ", enchantmentLevel, statTracker, (Integer.valueOf(enchantmentLevel * Enchantments.StatTracker.getPrice())), false);
+        return enchantment(balance, ChatColor.GREEN.toString() + ChatColor.BOLD + "Stat Tracker ", enchantmentLevel, statTracker, (Integer.valueOf(enchantmentLevel * Enchantments.StatTracker.getPrice())), true);
+    }
+
+    private ItemStack enchantment(int balance, String enchantmentName, int enchantmentLevel, TextComponent[] enchantmentLore, int enchantmentPrice, boolean maxed) {
+        ItemStack enchantmentItem = new ItemStack(Material.BOOK);
+        if (maxed)
+            enchantmentItem = new ItemStack(Material.ENCHANTED_BOOK);
         final ItemMeta enchantmentItemItemMeta = enchantmentItem.getItemMeta();
-        enchantmentItemItemMeta.setDisplayName(enchantmentName + enchantmentLevel);
+
+        if (maxed)
+            enchantmentItemItemMeta.setDisplayName(enchantmentName + ChatColor.DARK_GRAY + "- " + ChatColor.YELLOW + ChatColor.BOLD + "MAXED");
+        else
+            enchantmentItemItemMeta.setDisplayName(enchantmentName + enchantmentLevel);
+
         if (enchantmentItem != null) {
             List<Component> lore = enchantmentItem.lore();
             if (lore == null) {
@@ -170,12 +226,13 @@ public class Enchanting extends BaseCommand {
             lore.add(Component.text(""));
             for (TextComponent enchLore : enchantmentLore)
                 lore.add(enchLore);
-            lore.add(Component.text(""));
-            lore.add(Component.text(ChatColor.WHITE + "Price: " + StatsDisplay.EXPERIENCE_AMOUNT.getName() + " " + numberFormat.numberFormat(enchantmentPrice)));
-
-            if (balance >= Integer.valueOf(enchantmentLevel * Enchantments.Experience.getPrice())) {
+            if (!maxed) {
                 lore.add(Component.text(""));
-                lore.add(Component.text(Prefix.PREFIX.getPrefix() + ChatColor.YELLOW + "Click to Purchase"));
+                lore.add(Component.text(ChatColor.WHITE + "Price: " + StatsDisplay.EXPERIENCE_AMOUNT.getName() + " " + numberFormat.numberFormat(enchantmentPrice)));
+                if (balance >= enchantmentPrice) {
+                    lore.add(Component.text(""));
+                    lore.add(Component.text(Prefix.PREFIX.getPrefix() + ChatColor.YELLOW + "Click to Purchase"));
+                }
             }
             enchantmentItemItemMeta.lore(lore);
             enchantmentItem.setItemMeta(enchantmentItemItemMeta);
